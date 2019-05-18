9d856f3d12671e71106f306d7e6aeb3621b5216b
Jorge Benitez <jbenitez@sodimac.cl>
2019-05-18 01:33:08 -0400

    agora si

---
+++ src/java/com/entity/todo/RegistroLoginFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entity.todo;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author jbenitez
+ */
+@Stateless
+public class RegistroLoginFacade extends AbstractFacade<RegistroLogin> {
+
+    @PersistenceContext(unitName = "RestaurantOfTheFuture3PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public RegistroLoginFacade() {
+        super(RegistroLogin.class);
+    }
+    
+}
